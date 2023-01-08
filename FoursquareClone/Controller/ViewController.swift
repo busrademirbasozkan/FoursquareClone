737fc239df6ba1baa7140a8044a514d058cd2134
busrademirbasozkan <busrademirbasozkan@gmail.com>
2023-01-08 21:03:23 +0300

    pod dosyası oluşturularak parse entegrasyonu yapıldı

---
+++ FoursquareClone/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Büşra Özkan on 8.01.2023.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
