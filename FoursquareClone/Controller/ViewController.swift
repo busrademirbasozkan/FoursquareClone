b2e4f73bcc1e37ef3ce54022cc31186faab6205e
busrademirbasozkan <busrademirbasozkan@gmail.com>
2023-01-09 17:48:59 +0300

    user oluşturularak parse içine kayıt işlemi yapıldı.

--- FoursquareClone/Controller/ViewController.swift
+++ FoursquareClone/Controller/ViewController.swift
@@ -10,11 +10,50 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func signInButton(_ sender: Any) {
+    }
+    
+    
+    //Sign Up İşlemleri
+    @IBAction func signUpButton(_ sender: Any) {
+        if userNameText.text != "" && passwordText.text != "" {
+            let user = PFUser()
+            user.username = userNameText.text!
+            user.password = passwordText.text!
+            
+            user.signUpInBackground { (success, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    //Segue
+                }
+            }
+        }else{
+            makeAlert(titleInput: "Error", messageInput: "UserName or Password is nil")
+        }
+        
+    }
+    
+    
+    //Hata Mesajı
+    func makeAlert(titleInput : String , messageInput : String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
