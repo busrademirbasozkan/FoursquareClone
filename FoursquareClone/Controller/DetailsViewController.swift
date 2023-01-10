d0cc5d8f50c4980236a1e622ac2d6c9ffd24e930
busrademirbasozkan <busrademirbasozkan@gmail.com>
2023-01-10 21:38:14 +0300

    Details arayüzü tamamlandı.

---
+++ FoursquareClone/Controller/DetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsViewController.swift
+//  FoursquareClone
+//
+//  Created by Büşra Özkan on 10.01.2023.
+//
+
+import UIKit
+import MapKit
+
+class DetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var detailsImageView: UIImageView!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var atmosphereLabel: UILabel!
+    @IBOutlet weak var detailsMapKit: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
