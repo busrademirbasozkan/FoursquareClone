9602201b1d4bc796399390b448c07b2c8970ba2e
busrademirbasozkan <busrademirbasozkan@gmail.com>
2023-01-12 00:19:16 +0300

    mapkit ayarları yapıldı.

--- FoursquareClone/Controller/MapViewController.swift
+++ FoursquareClone/Controller/MapViewController.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapKit: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //MapKit ayarları
+        mapKit.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
 
+        //UIBarButtonItemler eklendi
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(title: "Save", style: UIBarButtonItem.Style.plain, target: self, action: #selector(saveClicked))
         
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector(cancelClicked))
@@ -29,5 +41,12 @@ class MapViewController: UIViewController {
         dismiss(animated: true)
     }
     
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapKit.setRegion(region, animated: true)
+    }
 
 }
